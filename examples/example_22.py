870508f69e6604d8daf157d3af1b9101331e8aa5
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-04-19 08:57:57 +0100

    Update example_22.py

--- examples/example_22.py
+++ examples/example_22.py
@@ -9,12 +9,12 @@ i = 1
 while i < len(expression):
     operator = expression[i]
     digit = int(expression[i+1])
-    
+
     if operator == '+':
         result += digit
     elif operator == '-':
         result -= digit
-    
+
     i += 2  # Move to the next operator
 
 # Print the final result
