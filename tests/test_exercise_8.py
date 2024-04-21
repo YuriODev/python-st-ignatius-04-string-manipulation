11145891f0e90ddeebc253b523cd1e1dca739974
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-21 17:01:52 +0100

    Update test_exercise_8.py

--- tests/test_exercise_8.py
+++ tests/test_exercise_8.py
@@ -2,6 +2,7 @@
 import unittest
 from .test_utils import CustomTestCase, CustomTestRunner
 
+
 class TestExercise8(CustomTestCase):
 
     def test_list_usage(self):
@@ -24,48 +25,54 @@ class TestExercise8(CustomTestCase):
 
     def test_case_1(self):
         """
-        Description of the test case 1
+        The specified letter is 'a' and the string is "Curiouser and curiouser!" cried Alice (she was so much surprised that for the moment she quite forgot how to speak good English).
         """
-        inputs = None
+        inputs = ['a', '"Curiouser and curiouser!" cried Alice (she was so much surprised that for the moment she quite forgot how to speak good English).']
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = '"Curiouser And curiouser!" cried Alice (she wAs so much surprised thAt for the moment she quite forgot how to speAk good English).'
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_2(self):
         """
-        Description of the test case 2
+        The specified letter is 'b' and the string is "The quick brown fox jumps over the lazy dog."
         """
-        inputs = None
+        inputs = ['a', "The quick brown fox jumps over the lazy dog."]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "The quick brown fox jumps over the lAzy dog."
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_3(self):
         """
-        Description of the test case 3
+        The specified letter is 'c' and the string is "Coding is fun!"
         """
-        inputs = None
+        inputs = ['c', "Coding is fun!"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "Coding is fun!"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_4(self):
         """
-        Description of the test case 4
+        The specified letter is 'd' and the string is "Don't worry, be happy!"
         """
-        inputs = None
+        inputs = ['a', "Don't worry, be happy!"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "Don't worry, be hAppy!"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_5(self):
         """
-        Description of the test case 5
+        The specified letter is 'e' and the string is "Everything is awesome!"
         """
-        inputs = None
+        inputs = ['e', "Everything is awesome!"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "EvErything is awEsomE!"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=CustomTestRunner())
