24fe0b8963c896e71e324683621128e78c0ebc72
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-22 17:03:59 +0100

    Update test_exercise_18.py

--- tests/test_exercise_18.py
+++ tests/test_exercise_18.py
@@ -2,6 +2,7 @@
 import unittest
 from .test_utils import CustomTestCase, CustomTestRunner
 
+
 class TestExercise18(CustomTestCase):
 
     def test_list_usage(self):
@@ -24,48 +25,64 @@ class TestExercise18(CustomTestCase):
 
     def test_case_1(self):
         """
-        Description of the test case 1
+        Test case 1: First character is '2' and second character is '5'
         """
-        inputs = None
+        inputs = ["2*5*7"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "70"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_2(self):
         """
-        Description of the test case 2
+        Test case 2: First character is '3' and second character is '4'
         """
-        inputs = None
+        inputs = ["3*4*6"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "72"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_3(self):
         """
-        Description of the test case 3
+        Test case 3: First character is '1' and second character is '9'
         """
-        inputs = None
+        inputs = ["1*9*8"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "72"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_4(self):
         """
-        Description of the test case 4
+        Test case 4: First character is '2' and second character is '3'
         """
-        inputs = None
+        inputs = ["2*3"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "6"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_5(self):
         """
-        Description of the test case 5
+        Test case 5: First character is '4' and second character is '5'
         """
-        inputs = None
+        inputs = ["4*5*6*7*8"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "6720"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
+    def test_case_6(self):
+        """
+        Test case 6: First character is '9' and second character is '9'
+        """
+        inputs = ["9*9*9*9*9*9*9"]
+        output = self.run_exercise(inputs)
+        expected_output = "4782969"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=CustomTestRunner())
