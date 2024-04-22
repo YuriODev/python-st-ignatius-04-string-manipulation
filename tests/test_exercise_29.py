5c2f26a0caea595eb5028f33dd04e1f2f333f81d
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-22 19:27:08 +0100

    Update test_exercise_29.py

--- tests/test_exercise_29.py
+++ tests/test_exercise_29.py
@@ -2,6 +2,7 @@
 import unittest
 from .test_utils import CustomTestCase, CustomTestRunner
 
+
 class TestExercise29(CustomTestCase):
 
     def test_list_usage(self):
@@ -22,50 +23,77 @@ class TestExercise29(CustomTestCase):
         """
         self.assertNotUseSelfDefinedFunctions()
 
+    # ## Exercise 29: Caesar Cipher - Hard 🥵 (Est. Time: 15-20 mins | Points: 30)
+
+    # **Problem:** Encrypt a string using the Caesar cipher, shifting letters by a specified number of places in the alphabet.
+
+    # ### Input:
+    # - An integer for the shift amount and a string to encrypt.
+
+    # ### Output:
+    # - The encrypted string.
+
+    # ### Examples:
+
+
+    # | No. | Inputs         | Outputs |
+    # | --- | -------------- | ------- |
+    # | 1   | 1<br>abc       | bcd     |
+    # | 2   | 26<br>abc      | abc     |
+    # | 3   | 1<br>Python    | Qzuipo  |
+    # | 4   | 3<br>Python    | Sbwkrq  |
+    # | 5   | 5<br>Python    | Udtqts  |
+
     def test_case_1(self):
         """
-        Description of the test case 1
+        Test case 1: Encrypting 'abc' with shift amount 1 should result in 'bcd'.
         """
-        inputs = None
+        inputs = ["1", "abc"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "bcd"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_2(self):
         """
-        Description of the test case 2
+        Test case 2: Encrypting 'abc' with shift amount 26 should result in 'abc'.
         """
-        inputs = None
+        inputs = ["26", "abc"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "abc"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_3(self):
         """
-        Description of the test case 3
+        Test case 3: Encrypting 'Python' with shift amount 1 should result in 'Qzuipo'.
         """
-        inputs = None
+        inputs = ["1", "Python"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "Qzuipo"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_4(self):
         """
-        Description of the test case 4
+        Test case 4: Encrypting 'Python' with shift amount 3 should result in 'Sbwkrq'.
         """
-        inputs = None
+        inputs = ["3", "Python"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "Sbwkrq"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_5(self):
         """
-        Description of the test case 5
+        Test case 5: Encrypting 'Python' with shift amount 5 should result in 'Udymts'.
         """
-        inputs = None
+        inputs = ["5", "Python"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "Udymts"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=CustomTestRunner())
