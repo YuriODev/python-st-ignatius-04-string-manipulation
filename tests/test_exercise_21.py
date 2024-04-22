9cb45b685e6aa45780f58716d3bbb6f9e68491f0
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-22 17:27:12 +0100

    Update test_exercise_21.py

--- tests/test_exercise_21.py
+++ tests/test_exercise_21.py
@@ -2,6 +2,7 @@
 import unittest
 from .test_utils import CustomTestCase, CustomTestRunner
 
+
 class TestExercise21(CustomTestCase):
 
     def test_list_usage(self):
@@ -24,48 +25,55 @@ class TestExercise21(CustomTestCase):
 
     def test_case_1(self):
         """
-        Description of the test case 1
+        Convert date format from '12/29/2022' to 'December 29, 2022'
         """
-        inputs = None
+        inputs = ["12/29/2022"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "December 29, 2022"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_2(self):
         """
-        Description of the test case 2
+        Convert date format from '03/04/2025' to 'March 04, 2025'
         """
-        inputs = None
+        inputs = ["03/04/2025"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "March 04, 2025"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_3(self):
         """
-        Description of the test case 3
+        Convert date format from '06/15/2030' to 'June 15, 2030'
         """
-        inputs = None
+        inputs = ["06/15/2030"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "June 15, 2030"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_4(self):
         """
-        Description of the test case 4
+        Convert date format from '09/01/2023' to 'September 01, 2023'
         """
-        inputs = None
+        inputs = ["09/01/2023"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "September 01, 2023"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
 
     def test_case_5(self):
         """
-        Description of the test case 5
+        Convert date format from '12/31/2024' to 'December 31, 2024'
         """
-        inputs = None
+        inputs = ["12/31/2024"]
         output = self.run_exercise(inputs)
-        expected_output = "None"
-        self.assertInCustom(expected=expected_output, actual=output, input_value=inputs)
+        expected_output = "December 31, 2024"
+        self.assertInCustom(expected=expected_output, actual=output,
+                            input_value=inputs)
+
 
 if __name__ == '__main__':
+    unittest.main()
     unittest.main(testRunner=CustomTestRunner())
