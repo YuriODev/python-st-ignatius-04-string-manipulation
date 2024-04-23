5bb0a664644ba59e90c96dc50bbd73792fa6a52c
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-23 08:34:29 +0100

    Update solution_26.py

--- solutions/solution_26.py
+++ solutions/solution_26.py
@@ -1,25 +1,3 @@
-# **Problem:** Print a part of the multiplication table for numbers in the range [a, b] across [c, d].
-
-# ### Input:
-# - Four integers a, b, c, and d defining the ranges.
-
-# ### Output:
-# - A formatted multiplication table.
-
-# ### Examples:
-
-# | No. | Inputs | Outputs |
-# | --- | ------ | ------- |
-# | 1   | 1<br>4<br>2<br>5 | Output 1         |
-
-# ### Output for Input Example 1
-
-# <pre>	2	3	4	5
-# 1	2	3	4	5
-# 2	4	6	8	10
-# 3	6	9	12	15
-# 4	8	12	16	20</pre>
-
 # Prompt the user to enter four integers separated by a space.
 print("Enter four integers: a, b, c, and d")
 
