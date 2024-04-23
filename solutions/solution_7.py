5e9cc18797dee7c39fe74631c845fc7f1d5324be
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-23 08:29:06 +0100

    Update solution_7.py

--- solutions/solution_7.py
+++ solutions/solution_7.py
@@ -1,14 +1,8 @@
-# # Read the input
-n = int(input())
+# Prompt the user to enter a string
+user_input = input("Enter a string: ")
 
-# Extract the digits
-d1 = n // 1000
-d2 = (n % 1000) // 100
-d3 = (n % 100) // 10
-d4 = n % 10
+# Replace all occurrences of the digit '4' with the word "Four"
+output = user_input.replace("4", "Four")
 
-# Calculate the sum of the digits
-sum_of_digits = d1 + d2 + d3 + d4
-
-# Print the sum of the digits
-print(sum_of_digits)
+# Output the modified string
+print(output)
