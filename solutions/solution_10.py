6436f8d5f4c04a5ccd444499e55376af83ad5ae7
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-23 08:33:05 +0100

    Update solution_10.py

--- solutions/solution_10.py
+++ solutions/solution_10.py
@@ -1,8 +1,8 @@
-# Read the input
-a = float(input())
+# Prompt the user for a three-digit integer
+number = input("Enter a three-digit integer: ")
 
-# Calculate the angle
-angle = (a % 30) * 12
+# Calculate the sum of the digits
+sum_of_digits = int(number[0]) + int(number[1]) + int(number[2])
 
-# Print the angle
-print(angle)
+# Output the sum of the digits
+print(sum_of_digits)
